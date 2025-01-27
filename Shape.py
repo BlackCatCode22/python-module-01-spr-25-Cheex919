bd689484eeac2bb875cd026150eebd9e188de80c
Chee Xiong <107579292+Cheex919@users.noreply.github.com>
2025-01-26 21:21:48 -0800

    Add files via upload

---
+++ Shape.py
@@ -0,0 +1,29 @@
+# Shapes
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+
+print("|   |")
+print("|___|")
+print("    |")
+print("    |")
+# Variable testing
+character_name = "Peter Parker"
+character_age = "17"
+print("Man gets bitten")
+print("his name is " + character_name + ", ")
+print("He becomes Spider-Man at the age of " +character_age+" and is my favorite Hero")
+
+# Test 1
+isMale = True
+isFemale = False
+# True false Variable, used often.
+
+# Quotation mark
+Phrase = "Hey Arnold"
+print("Hey\"Arnold\"")
+print(Phrase + " the cartoon")
+print(Phrase.upper())
+
